d8439a7d22f0a13da4cd0237ac94f3e7d8229bfe
Ernestinoh <tsimbinaernestino@gmail.com>
2025-08-06 22:33:57 +0300

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -5,8 +5,29 @@ toggle.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
 
   if (body.classList.contains('dark-mode')) {
-    toggle.textContent = '☀️ Mode clair';
+    toggle.textContent = '☀️ ';
   } else {
-    toggle.textContent = '🌙 Mode sombre';
+    toggle.textContent = '🌙 ';
   }
+});
+
+// Afficher le bouton retour en haut
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  scrollTopBtn.style.display = window.scrollY > 200 ? "block" : "none";
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+// Soumission du formulaire
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Merci pour votre message !");
+  this.reset();
 });
